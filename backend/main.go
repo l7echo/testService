1d38ecc2ed00b4ceb541c539e69729fe6b18fdbb
Pecho <pecho@2ba.su>
2021-10-21 15:39:13 +0300

    get test connection to db

--- backend/main.go
+++ backend/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Record struct {
@@ -77,6 +81,68 @@ func deleteById(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	// open db connection
+	db, err := sql.Open("mysql", "root:pa55w0rd@tcp(localhost:3306)/test_db")
+	if err != nil {
+		panic(err.Error())
+	}
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+	defer db.Close()
+
+	//err = db.Ping()
+	//if err != nil {
+	//	panic(err.Error())
+	//}
+
+	// exec query
+	rows, err := db.Query("select * from content")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//make slice for the values
+	values := make([]sql.RawBytes, len(columns))
+
+	// row.Scan wants []inteface{} as an argument, so we must copy the references into such a slice
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	// fetch rows
+	for rows.Next() {
+		// get raw bytes from data
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// now do smth with data
+		// print, for example
+		var value string
+		for i, col := range values {
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			fmt.Println(columns[i], ": ", value)
+		}
+		fmt.Println("--------------------------------")
+	}
+	if err = rows.Err(); err != nil {
+		panic(err.Error())
+	}
+
 	r := mux.NewRouter()
 
 	dbContent = append(dbContent, Record{Id: "1", Value: "v1"})
