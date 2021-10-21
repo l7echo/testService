4001c5158911ca71a39c08b85cb6a78b6f5d02b7
Pecho <pecho@2ba.su>
2021-10-22 00:22:21 +0300

    create backend container; all works; need to switch from json to env

--- database/install_test_db.sh
+++ database/install_test_db.sh
@@ -1,5 +1,5 @@
 docker pull mysql
 docker-compose -f docker-compose.mysql.yml up -d
 sleep 25s
-docker exec -it bin_db_1 bash -c "mysql -uroot -ppa55w0rd test_db < /test_db/test_db.sql"
+docker exec -it mysqlTestDB bash -c "mysql -uroot -ppa55w0rd test_db < /test_db/test_db.sql"
 echo "done"
